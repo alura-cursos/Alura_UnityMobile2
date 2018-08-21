c71707ed3f06787f1e97a5b60428e48ec713ea62
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-08-21 11:01:05 -0300

    Video 2.2

---
+++ Assets/Scritpt/Gameplay/ReservaDeInimigos.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReservaDeInimigos : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject prefab;
+    [SerializeField]
+    private int quantidade;
+
+    private Stack<GameObject> reservaDeInimigos;
+
+    private void Start () {
+        this.reservaDeInimigos = new Stack<GameObject>();
+        this.CriarTodosOsInimigos();
+	}
+	
+    private void CriarTodosOsInimigos()
+    {
+        for(var i =0; i<this.quantidade; i++)
+        {
+            var inimigo = GameObject.Instantiate(this.prefab, this.transform);
+            inimigo.SetActive(false);
+            this.reservaDeInimigos.Push(inimigo);
+        }
+    }
+}
