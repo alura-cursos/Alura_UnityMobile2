bdc38f1b8d21c9d7aecf67208c7ebb2c3587e170
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-08-24 09:58:28 -0300

    Video4.3

---
+++ Assets/Scritpt/VolumeDoMixer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeDoMixer : MonoBehaviour {
+    [SerializeField]
+    private AudioMixer mixer;
+    [SerializeField]
+    private string parametroDoMixer;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey(this.parametroDoMixer))
+        {
+            this.mixer.SetFloat(this.parametroDoMixer, PlayerPrefs.GetFloat(this.parametroDoMixer));
+        }
+        else
+        {
+            this.mixer.SetFloat(this.parametroDoMixer, 0);
+        }
+    }
+    public void MudarVolume(float volume)
+    {
+        this.mixer.SetFloat(this.parametroDoMixer, volume);
+        PlayerPrefs.SetFloat(this.parametroDoMixer, volume);
+    }
+}
