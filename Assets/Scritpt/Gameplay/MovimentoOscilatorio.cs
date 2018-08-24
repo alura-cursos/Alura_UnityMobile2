5bca37930f4292a2a45bc977266acf28d6700ea6
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-08-24 10:46:04 -0300

    Video5.2

---
+++ Assets/Scritpt/Gameplay/MovimentoOscilatorio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoOscilatorio : MonoBehaviour {
+
+    
+    [SerializeField]
+    private float amplitude;
+    [SerializeField]
+    private float velocidade;
+
+    private Vector3 posicaoInicial;
+    private float angulo;
+    private void Awake()
+    {
+        this.posicaoInicial = this.transform.position;
+    }
+
+    private void Update () {
+        this.angulo += this.velocidade;
+        var variacao = Mathf.Sin(angulo);
+        this.transform.position = this.posicaoInicial + (this.amplitude * variacao * Vector3.up);
+	}
+}
